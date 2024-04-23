a3dd120b8dc561034beacdfd38660ee87c2f6270
Rocky Dey <coderrocky25@gmail.com>
2024-04-23 10:52:05 +0600

    started working on new project called coffee-tea-house and installed all important libraries

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      color1: "#fff",
+      color2: "#242222",
+      color3: "#E3B577",
+      color4: "#331A15",
+      color5: "#1B1A1A",
+      color6: "#1B1A1A",
+    },
+    extend: {
+      fontFamily: {
+        fontFamily: {
+          raleway: '"Raleway", sans-serif',
+          rancho: '"Rancho", cursive',
+        },
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
+};
